301708ceee52139d2b0dcbfb4fd4be42fe8e2fb9
lijx235 <lijx235@chinaunicom.cn>
2020-01-07 10:39:35 +0800

    eureka微服务注册中心搭建

---
+++ regist_center/src/test/java/com/ljx/eureka/regist_center/RegistCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljx.eureka.regist_center;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
