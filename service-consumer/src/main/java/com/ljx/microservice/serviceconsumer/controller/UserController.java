10c99e4038c3dca92b5e0f57fcadec16106d2ade
lijx235 <ljx9653@163.com>
2020-09-30 16:16:47 +0800

    hystrix使用

--- service-consumer/src/main/java/com/ljx/microservice/serviceconsumer/controller/UserController.java
+++ service-consumer/src/main/java/com/ljx/microservice/serviceconsumer/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ljx.microservice.serviceconsumer.controller;
 
-import com.ljx.microservice.serviceconsumer.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -18,6 +19,9 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("consumer/user")
+//定义全局的降级方法，注意：在这里定义了之后，类中方法不加@HystrixCommand是无法使用全局降级方法的，如果不行使用全局降级方法，可以在@HystrixCommand中指定自己的fallbackMethod
+@DefaultProperties(defaultFallback = "defaultFallbackMethod")
+@SuppressWarnings("all")
 public class UserController {
     @Autowired
     private RestTemplate restTemplate;
@@ -27,9 +31,13 @@ public class UserController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping
-    public User queryUserById(@RequestParam("id") Long id){
+    //指定降级方法
+    @HystrixCommand(fallbackMethod = "queryUserByIdFallback")
+    //进行降级方法的改造，在业务层面，降级方法返回User对象是不合适的，因此返回值改为String
+    //public User queryUserById(@RequestParam("id") Long id){
+    public String queryUserById(@RequestParam("id") Long id) {
         //注意此处硬编码，后期要进行改造
-//        User user = this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
+        //User user = this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
         /**
          * 服务消费方硬编码改造，改成eureka的地址
          */
@@ -40,7 +48,34 @@ public class UserController {
         User user = this.restTemplate.getForObject("http://"+providerserviceInstance.getHost()+":"+providerserviceInstance.getPort()+"/user/" + id, User.class);*/
         //不再使用上面那种通过拼接url的方式，而是用服务名代替原来的ip和port，ribbon会自动解析服务名，并找到合适的服务实例进行调用
         String baseUrl = "http://service-provider/user/" + id;
-        User user = this.restTemplate.getForObject(baseUrl, User.class);
-        return user;
+        /*User user = this.restTemplate.getForObject(baseUrl, User.class);
+        return user; //进行降级方法的改造*/
+        return this.restTemplate.getForObject(baseUrl, String.class);
+    }
+
+    @HystrixCommand
+    @RequestMapping("test1")
+    public String method1() {
+        try {
+            //降级时间为6s，手动触发降级
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+        }
+        return "使用全局降级方法";
+    }
+
+    @RequestMapping("test2")
+    public String method2() {
+        return "不使用降级方法";
+    }
+
+    //全局降级方法，注意参数列表要为空，返回参数保持一致
+    public String defaultFallbackMethod() {
+        return "全局降级方法触发";
+    }
+
+    //指定方法的降级方法，要求：参数列表和返回值需要和原方法一致
+    public String queryUserByIdFallback(Long id) {
+        return "服务正忙，请稍后再试";
     }
 }
